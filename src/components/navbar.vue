4d21e6dc70b102d7030ccd88bd8eb38c3b903398
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-03-09 00:54:42 -0500

    refactor: crteate navbar component

---
+++ src/components/navbar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import CreateCategory from '@/components/create-category.vue'
+</script>
+
+<template>
+  <nav class="navbar">
+    <button class="back-button">
+      <RouterLink to="/">Back to Home Page</RouterLink>
+    </button>
+    <slot><CreateCategory /></slot>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.7);
+  padding: 15px 30px;
+  border-radius: 10px;
+}
+
+.back-button {
+  background: transparent;
+  border: none;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.back-button a {
+  text-decoration: none;
+  color: white;
+}
+
+.back-button:hover a {
+  text-decoration: underline;
+}
+</style>
